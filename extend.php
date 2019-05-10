0381bc2b126af202ffd9f750921f64e3685a9e1f
Clark Winkelmann <clark.winkelmann@gmail.com>
2019-05-10 17:36:23 +0200

    Style for active links, similar to active links in sidebar

--- extend.php
+++ extend.php
@@ -18,7 +18,8 @@ return [
     new Extend\Locales(__DIR__.'/locale'),
 
     (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/js/dist/forum.js'),
+        ->js(__DIR__.'/js/dist/forum.js')
+        ->css(__DIR__.'/less/forum.less'),
 
     (new Extend\Frontend('admin'))
         ->js(__DIR__.'/js/dist/admin.js')
