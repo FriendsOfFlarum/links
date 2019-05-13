2996e280a5adfbfc4a6ce6002186423035fff14e
Clark Winkelmann <clark.winkelmann@gmail.com>
2019-05-13 02:18:50 +0200

    Active links (#5)
    * Add active class to links
    
    * Style for active links, similar to active links in sidebar
    
    * Fix less indentation
    
    * Recompile dist, for some reason it wasn't up to date
    
    * Update editorconfig to 4 spaces for js, like the linter applies
    
    * Run build command

--- js/src/forum/components/LinkItem.js
+++ js/src/forum/components/LinkItem.js
@@ -1,13 +1,26 @@
 /* global m*/
 
+import app from 'flarum/app';
 import LinkButton from 'flarum/components/LinkButton';
 
 export default class LinkItem extends LinkButton {
     view() {
         const link = this.props.link;
+        let className = 'LinksButton Button Button--link';
+
+        if (link.isInternal()) {
+            const currentPath = m.route();
+            const linkPath = link.url().replace(app.forum.attribute('baseUrl'), '');
+            // The link is active if the current path starts with the link path.
+            // Except if it's the base url, in which case only an exact match is considered active
+            if (currentPath.indexOf(linkPath) === 0 && (currentPath === '/' || linkPath !== '/')) {
+                className += ' active';
+            }
+        }
+
         return (
             <a
-                className="LinksButton Button Button--link"
+                className={className}
                 target={link.isNewtab() ? '_blank' : ''}
                 config={link.isInternal() ? m.route : ''}
                 href={link.url()}
