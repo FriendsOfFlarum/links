b5aee8948f81ffaed3abeeb0d9a6674bbce1e6ac
Ian Morland <ianm@giffgaff.co.uk>
2020-05-04 11:36:30 +0100

    Replace Zend with Laminas

--- src/Api/Controller/OrderLinksController.php
+++ src/Api/Controller/OrderLinksController.php
@@ -16,7 +16,7 @@ use FoF\Links\Link;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Zend\Diactoros\Response\EmptyResponse;
+use Laminas\Diactoros\Response\EmptyResponse;
 
 class OrderLinksController implements RequestHandlerInterface
 {
