e4cf0f168cfa084e5d9a5959ef3c31a89a70ea7c
karaok491 <karaok491@users.noreply.github.com>
2020-12-09 01:26:55 +0000

    Apply fixes from StyleCI

--- src/LoadForumLinksRelationship.php
+++ src/LoadForumLinksRelationship.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of fof/links.
+ *
+ * Copyright (c) 2019 FriendsOfFlarum.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace FoF\Links;
 
 use Flarum\Api\Controller\ShowForumController;
