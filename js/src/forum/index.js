8859e17d40ff2c84dc187516c3a1a4e12fb9daca
David Wheatley <hi@davwheat.dev>
2021-09-02 19:01:28 +0100

    Revert "Merge branch 'dw/wip-header-break'"
    This reverts commit bb4f2f55255b07e8cf273fd4ff96694052d12e42.

---
+++ js/src/forum/index.js
@@ -0,0 +1,26 @@
+import { extend } from 'flarum/common/extend';
+import HeaderPrimary from 'flarum/common/components/HeaderPrimary';
+
+import Link from '../common/models/Link';
+import LinkItem from './components/LinkItem';
+import LinkDropdown from './components/LinkDropdown';
+import sortLinks from '../common/utils/sortLinks';
+
+export * from './components';
+export * from '../common/utils';
+export * from '../common/models';
+
+app.initializers.add('fof-links', () => {
+  app.store.models.links = Link;
+
+  extend(HeaderPrimary.prototype, 'items', (items) => {
+    const links = app.store.all('links').filter((link) => !link.isChild());
+    const addLink = (parent) => {
+      const hasChildren = !!app.store.all('links').filter((link) => link.parent() == parent).length;
+
+      items.add(`link${parent.id()}`, hasChildren ? LinkDropdown.component({ link: parent }) : LinkItem.component({ link: parent }));
+    };
+
+    sortLinks(links).map(addLink);
+  });
+});
