5d892484b8e81794dddc88ac55b50edf6824ffd1
Rafał Całka <25438601+rafaucau@users.noreply.github.com>
2026-09-13 23:34:43 +0200

    chore: switch to pnpm

--- js/jest.config.cjs
+++ js/jest.config.cjs
@@ -1,1 +1,11 @@
-module.exports = require('@flarum/jest-config')();
+const path = require('path');
+
+module.exports = require('@flarum/jest-config')({
+  // Allow the config's helpers to be transformed through pnpm's nested layout.
+  transformIgnorePatterns: ['/node_modules/(?!(?:\\.pnpm/|@flarum/jest-config/))', '\\.pnp\\.[^\\/]+$'],
+  modulePaths: [
+    path.join(__dirname, 'node_modules'),
+    // Vendored core also needs access to the config's runtime dependencies.
+    path.resolve(path.dirname(require.resolve('@flarum/jest-config')), '../..'),
+  ],
+});
